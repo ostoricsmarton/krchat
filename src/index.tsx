f54671b478f0d5ba85afe78fbe8f8152d9245342
Egy név <valamilyen@email>
2025-11-13 17:46:08 +0100

    kilencedik

--- src/index.tsx
+++ src/index.tsx
@@ -6,26 +6,18 @@ import { useEffect, useState } from 'preact/hooks';
 import { chatService } from './ChatService';
 import "./Pwa"
 
-if ('Notification' in window) {
-  Notification.requestPermission().then((permission) => {
-    console.log('Notification permission:', permission);
-    // Itt: "granted", "denied" vagy "default"
-  });
-} else {
-  console.warn('A Notification API nem támogatott ebben a böngészőben.');
-}
-function App()
-{
-	let [ renderCount, setRenderCount ] = useState( 1 );
-	console.log( "App render count: " + renderCount );
-	useEffect( () =>
-	{
-		const listener = () => setRenderCount( x => x + 1 );
-		chatService.addListener( listener );
-		return () => chatService.removeListener( listener );
-	}, [] );
+
+function App() {
+	let [renderCount, setRenderCount] = useState(1);
+	console.log("App render count: " + renderCount);
+	Notification.requestPermission()
+	useEffect(() => {
+		const listener = () => setRenderCount(x => x + 1);
+		chatService.addListener(listener);
+		return () => chatService.removeListener(listener);
+	}, []);
 	return chatService.inbox ? <Main /> : <Login />
 }
 
 
-render( <App />, document.getElementById( 'app' ) );
+render(<App />, document.getElementById('app'));
